4ce5655811b8df326e5ddad10aaf330e257b1862
meattree0 <coolich.ya@gmail.com>
2025-07-10 01:31:21 +0300

    try fix;

--- server/tests/unit/test_appointments_api.py
+++ server/tests/unit/test_appointments_api.py
@@ -3,16 +3,14 @@ from datetime import datetime
 import pytest
 from fastapi import HTTPException
 
-from server.main import getapp
+from server.main import postapp
 from server.schema import AppointmentModel
 
 
 @pytest.mark.asyncio
 async def test_appointment_time_conflict(mocker):
-    """Тестирование обработки конфликта времени при создании записи"""
     mock_conn = mocker.AsyncMock()
-
-    mock_conn.fetchval.return_value = 1
+    mock_conn.fetchval.side_effect = [1]
 
     appointment = AppointmentModel(
         doctor_id=1,
@@ -22,19 +20,6 @@ async def test_appointment_time_conflict(mocker):
     )
 
     with pytest.raises(HTTPException) as exc_info:
-        await getapp(appointment, mock_conn)
+        await postapp(appointment, mock_conn)
 
     assert exc_info.value.status_code == 409
-
-    query = (
-        "SELECT FROM appointment "
-        "WHERE doctor_id = $1 AND start_time < $3 AND end_time > $2 "
-        "LIMIT 1;"
-    )
-
-    mock_conn.fetchval.assert_called_once_with(
-        query,
-        1,
-        appointment.start_time,
-        appointment.end_time,
-    )
