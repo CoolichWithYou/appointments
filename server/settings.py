89949df63622650ed71f9465b346f1a7efe4fc9a
meattree0 <coolich.ya@gmail.com>
2025-07-10 13:05:08 +0300

    add activity, architecture diagrams; add answers; make other minor changes;

--- server/settings.py
+++ server/settings.py
@@ -7,12 +7,12 @@ from server.utility import singleton
 class Settings(BaseSettings):
     DB_HOST: str
     DB_PORT: str
-    DB_NAME: str
-    DB_USER: str
-    DB_PASSWORD: str
+    POSTGRES_DB: str
+    POSTGRES_USER: str
+    POSTGRES_PASSWORD: str
 
     def get_connection(self):
         return (
-            f"postgres://{self.DB_USER}:{self.DB_PASSWORD}"
-            f"@{self.DB_HOST}:{self.DB_PORT}/{self.DB_NAME}"
+            f"postgres://{self.POSTGRES_USER}:{self.POSTGRES_PASSWORD}"
+            f"@{self.DB_HOST}:{self.DB_PORT}/{self.POSTGRES_DB}"
         )
