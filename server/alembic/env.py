f0ef3b1720b3def4f35be77d3238de8287350e6a
meattree0 <coolich.ya@gmail.com>
2025-07-13 02:07:48 +0300

    update makefile; make other minor formatting changes;

--- server/alembic/env.py
+++ server/alembic/env.py
@@ -2,13 +2,12 @@ import os
 from logging.config import fileConfig
 
 from dotenv import load_dotenv
-from sqlalchemy import engine_from_config
-from sqlalchemy import pool
-
-from alembic import context
+from sqlalchemy import engine_from_config, pool
 from sqlmodel import SQLModel
 
-load_dotenv('.env.example')
+from alembic import context
+
+load_dotenv(".env.example")
 
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
@@ -89,9 +88,7 @@ def run_migrations_online() -> None:
     )
 
     with connectable.connect() as connection:
-        context.configure(
-            connection=connection, target_metadata=target_metadata
-        )
+        context.configure(connection=connection, target_metadata=target_metadata)
 
         with context.begin_transaction():
             context.run_migrations()
