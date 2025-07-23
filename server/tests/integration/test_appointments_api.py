c00c31585190ae76f96f6ed4c0c9e20935662e07
meattree0 <coolich.ya@gmail.com>
2025-07-23 17:54:24 +0300

    try fix;

--- server/tests/integration/test_appointments_api.py
+++ server/tests/integration/test_appointments_api.py
@@ -1,48 +1,34 @@
 from datetime import datetime
 
-from fastapi.testclient import TestClient
-from sqlalchemy import StaticPool
-from sqlmodel import Session, SQLModel, create_engine
+import pytest
+from httpx import AsyncClient, ASGITransport
 
-from server.main import app, get_session
+from server.main import app
 
+@pytest.mark.asyncio
+async def test_test_intersection():
+    async with AsyncClient(
+            transport=ASGITransport(app=app), base_url="http://test"
+    ) as ac:
 
-def test_test_intersection():
-    engine = create_engine(
-        "sqlite://",
-        connect_args={"check_same_thread": False},
-        poolclass=StaticPool,
-    )
-    SQLModel.metadata.create_all(engine)
+        response = await ac.post("/patient", json={"name": "Ivan"})
 
-    with Session(engine) as session:
+        patient = response.json()
 
-        def get_session_override():
-            return session
-
-        app.dependency_overrides[get_session] = get_session_override
-
-        client = TestClient(app)
-
-        response = client.post("/patient", json={"name": "Ivan"})
-
-        data = response.json()
-
-        assert data["name"] == "Ivan"
+        assert patient["name"] == "Ivan"
         assert response.status_code == 200
 
-        response = client.post("/doctor", json={"full_name": "Dmitry"})
+        response = await ac.post("/doctor", json={"full_name": "Dmitry"})
 
-        data = response.json()
+        doctor = response.json()
 
-        assert data["full_name"] == "Dmitry"
+        assert doctor["full_name"] == "Dmitry"
         assert response.status_code == 200
-
-        response = client.post(
+        response = await ac.post(
             "/appointment",
             json={
-                "doctor_id": 1,
-                "patient_id": 1,
+                "doctor_id": doctor["id"],
+                "patient_id": patient["id"],
                 "start_time": datetime(2025, 1, 1, 10, 0).isoformat(),
                 "end_time": datetime(2025, 1, 1, 11, 0).isoformat(),
             },
@@ -50,7 +36,7 @@ def test_test_intersection():
 
         assert response.status_code == 200
 
-        response = client.post(
+        response = await ac.post(
             "/appointment",
             json={
                 "doctor_id": 1,
@@ -61,5 +47,3 @@ def test_test_intersection():
         )
 
         assert response.status_code == 409
-
-        app.dependency_overrides.clear()
--- server/tests/unit/test_appointments_api.py
+++ server/tests/unit/test_appointments_api.py
@@ -1,14 +1,6 @@
 import pytest
-from httpx import ASGITransport, AsyncClient
-
-from server.main import app
 
 
-@pytest.mark.anyio
-async def test_root():
-    async with AsyncClient(
-        transport=ASGITransport(app=app), base_url="http://test"
-    ) as ac:
-        response = await ac.get("/health")
-    assert response.status_code == 200
-    assert response.json() == {"status": "ok"}
+@pytest.mark.asyncio
+async def test_doctor_appointments_overlap_found():
+    pass
